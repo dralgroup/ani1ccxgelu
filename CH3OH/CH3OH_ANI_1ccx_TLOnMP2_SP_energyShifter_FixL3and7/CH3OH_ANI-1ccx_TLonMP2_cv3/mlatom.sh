5563dbd85e157ce3454a499c78af9b7d75a0b866
sfatemealavi <s.fatemealavi@outlook.com>
2024-12-06 15:44:08 +0800

    Add files via upload

---
+++ CH3OH/CH3OH_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL3and7/CH3OH_ANI-1ccx_TLonMP2_cv3/mlatom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J fateme
+#SBATCH -p gpu
+#SBATCH -c 1
+#SBATCH -G 1
+#SBATCH -o Job%J_%x_%N_out
+#SBATCH -e Job%J_%x_%N_err
+#SBATCH -w b008
+
+
+#----> Job begins <----
+source ~/.bashrc
+export PYTHONPATH="/mlatom/home/fatemealavi/mlatom_devBranch"
+python train.py > output_cv3
