60e68117658601d79016f78635e73378b3c105e0
sfatemealavi <s.fatemealavi@outlook.com>
2024-12-06 17:12:37 +0800

    Add files via upload

---
+++ HNO2/HNO2_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL1and3/freq_HNO2_by_TL_ani_1ccx/gaussian.com
@@ -0,0 +1,11 @@
+%nproc=1
+#p freq external='/mlatom/software/miniconda3/bin/python /mlatom/home/fatemealavi/mlatom_devBranch/mlatom/interfaces/gaussian_interface.py model_predict_kwargs'
+
+7652aab6-e28b-4fe5-9eb4-01a9534967b9
+
+0 1
+N             0.0000000000000           0.5210178151348           0.0000000000000
+O            -1.1175469322318           0.1531428906808           0.0000000000000
+O             0.8975893861818          -0.5908305287677          -0.0000000000000
+H             1.7596603683988          -0.1456236012440           0.0000000000000
+
---
+++ HNO2/HNO2_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL1and3/freq_HNO2_by_TL_ani_1ccx/master.py
@@ -0,0 +1,106 @@
+import sys
+sys.path.append('/mlatom/home/fatemealavi/mlatom_devBranch')
+import mlatom as ml
+import mkl # required for KREG_API models
+import matplotlib.pyplot as plt
+import numpy as np  
+import subprocess
+import json as json
+import csv
+import tabulate
+import pandas as pd
+import torch
+import torchani
+import os
+import math 
+from tabulate import tabulate
+
+print("ml:\n", ml)
+molecule_formula = 'HNO2' 
+level_of_theory = "MP2"
+
+filename = "/mlatom/home/fatemealavi/anharmonic/data/hono_mp2_avtz_6406.npz"
+data = np.load(filename)
+labeled_database = ml.molecular_database.from_numpy(data['R'], data['Z'])
+labeled_database.add_scalar_properties(data["E"] * 0.0367492929, property_name = "energy")
+labeled_database.add_xyz_derivative_properties(-1 * data["F"] * 0.0367492929, property_name = "energy", xyz_derivative_property = "energy_gradients")
+
+for mol in labeled_database.molecules:
+    iflag = 0
+    for iatom in range(len(mol.atoms)):
+        if mol.atoms[iatom].element_symbol == "X":
+            del(mol.atoms[iatom-iflag])
+            iflag += 1
+
+print("************************ ANI-1ccx_GELU **************************")
+ani_1ccx_gelu = ml.models.model_tree_node(
+    name='ani_gelu', 
+    children=[ml.models.model_tree_node(
+        name=f'nn{ii}',
+        model=ml.models.ani(model_file=f'/mlatom/home/fatemealavi/anharmonic/ANI_1ccx_HNO2_new_model/HNO2_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL1and3/ani1ccx_gelu_TL_ani1x_gelu_byFateme/cv{ii}.pt'),
+        operator='predict') for ii in range(8)],
+    operator='average'
+)
+
+geoOptimization_anni1ccxGelu = ml.simulations.optimize_geometry(model = ani_1ccx_gelu, initial_molecule = labeled_database[0], program = 'gaussian')
+optmol_ani1ccxGelu = geoOptimization_anni1ccxGelu.optimized_molecule
+print("Optimized Geometry for Ani_1ccx_gelu:\n", optmol_ani1ccxGelu.get_xyz_coordinates())
+
+ml.simulations.freq(model = ani_1ccx_gelu, program = 'gaussian', molecule = optmol_ani1ccxGelu, anharmonic = False)
+frequencies_ani1ccxGelu = optmol_ani1ccxGelu.frequencies
+numOfAtoms = len(labeled_database.molecules[0])
+print("elements: ", labeled_database.molecules[0])
+modes = [i + 1 for i in range(numOfAtoms * 3 - 6)]  # Nonlinear molecules
+df_freq = pd.DataFrame(modes, columns = ["mode"])
+df_freq["freq_ANI_1ccx_gelu"] = frequencies_ani1ccxGelu
+
+print("****************** Frequencies of All Models **********************")
+def update_database(df_freq):
+    methods = ["exp", "mp2", "physnet_MP2_NN1", "physnet_MP2_NN2", "physnet_CCSD(T)_NN1", "physnet_CCSD(T)_NN2", "physnet_CCSD(T)_F12_NN1", "physnet_CCSD(T)_F12_NN2"]
+    df = pd.read_csv("/mlatom/home/fatemealavi/anharmonic/ANI_1ccx_HNO2_new_model/HNO2_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL1and3/freq_HNO2_by_TL_ani_1ccx/freq_otherMethods.csv", sep = ",", header = 0)
+    for meth in methods:    
+        df_freq = pd.concat([df_freq, df[meth]], axis = 1)
+    df_freq.set_index("mode", inplace = True)
+    colHeaders = list(df_freq.columns.values)
+    
+    freqRMSEexpList = []
+    freqMAEexpList = []
+    freqRMSEmp2List = []
+    freqMAEmp2List = []
+    for col in colHeaders:
+        freqRMSEexp = 0
+        freqMAEexp = 0
+        freqRMSEmp2 = 0
+        freqMAEmp2 = 0
+        for ind in modes:
+            freqRMSEexp += (df_freq["exp"][ind] - df_freq[col][ind])**2
+            freqMAEexp += abs(df_freq["exp"][ind] - df_freq[col][ind]) 
+            freqRMSEmp2 += (df_freq["mp2"][ind] - df_freq[col][ind])**2
+            freqMAEmp2 += abs(df_freq["mp2"][ind] - df_freq[col][ind])
+        freqRMSEexp /= len(df_freq.index)
+        freqRMSEexp = math.sqrt(freqRMSEexp)
+        freqMAEexp /= len(df_freq.index)
+        freqRMSEmp2 /= len(df_freq.index)
+        freqRMSEmp2 = math.sqrt(freqRMSEmp2)
+        freqMAEmp2 /= len(df_freq.index)
+        freqRMSEexpList.append(freqRMSEexp)
+        freqMAEexpList.append(freqMAEexp)     
+        freqRMSEmp2List.append(freqRMSEmp2)
+        freqMAEmp2List.append(freqMAEmp2)   
+    df_freq.loc["MAE(exp)"] = freqMAEexpList 
+    df_freq.loc["RMSE(exp)"] = freqRMSEexpList  
+    df_freq.loc["MAE(mp2)"] = freqMAEmp2List 
+    df_freq.loc["RMSE(mp2)"] = freqRMSEmp2List  
+    
+    return df_freq
+
+freqOfDiffMethods = update_database(df_freq)
+
+print("\n************* Frequency Table *****************")
+def generate_table_freq(freqOfDiffMethods):
+    return tabulate(freqOfDiffMethods, headers = "keys", tablefmt = "psq1")
+tableOfFreq = generate_table_freq(freqOfDiffMethods)
+print("Frequencies of different models:\n", tableOfFreq)
+with open ("/mlatom/home/fatemealavi/anharmonic/ANI_1ccx_HNO2_new_model/HNO2_ANI_1ccx_TLOnMP2_SP_energyShifter_FixL1and3/freq_HNO2_by_TL_ani_1ccx/harmonicfreq.txt", "w") as file1:
+    file1.write(tableOfFreq)
+    
