044d04e7b7508b54868f2431126b67f09b300e78
Seyedeh Fatemeh Alavi <s.fatemealavi@outlook.com>
2024-12-26 12:42:34 +0800

    Add files via upload

---
+++ CH2O/H2CO_ani_1ccx_gelu_TL_CCSD_T_SP_energyShifter_FixL1and7/H2CO_ani_1ccx_gelu_TL_CCSD_T_cv2/mlatom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J fateme
+#SBATCH -p gpu
+#SBATCH -c 20
+#SBATCH -o Job%J_%x_%N_out
+#SBATCH -e Job%J_%x_%N_err
+#SBATCH -w b008
+
+
+#----> Job begins <----
+source ~/.bashrc
+export PYTHONPATH="/mlatom/home/fatemealavi/mlatom_devBranch"
+python train.py > output_cv2
